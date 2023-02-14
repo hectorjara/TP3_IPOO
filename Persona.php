d978b34662a38d31fcce029b1597073087fa563f
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2023-02-14 01:17:45 -0300

    Create Persona.php
    Clase Persona creada

---
+++ Persona.php
@@ -0,0 +1,39 @@
+<?php
+Class Persona {
+	
+	private $dni, $nombre, $apellido;
+
+	public function __construct($dni, $nombre, $apellido){
+		$this->dni = $dni;
+        $this->nombre = $nombre;
+		$this->apellido = $apellido;
+	}
+
+    public function getDNI(){
+		return $this->dni;
+	}
+	public function setDNI($dni){
+		$this->dni = $dni;
+	}
+
+	public function getNombre(){
+		return $this->nombre;
+	}
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+	
+	public function getApellido(){
+		return $this->apellido;
+	}
+	public function setApellido($apellido){
+		$this->apellido = $apellido;
+	}
+		
+	public function __toString(){
+		$cadena = "Dni: ". $this->getDNI().", Nombre: ".$this->getNombre().", Apellido: ".$this->getApellido().".";
+		return $cadena;
+	}
+	
+	
+}
